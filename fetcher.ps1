ff00e7c6aa4fdaa8154f5b4b75151272dd4ee0e4
bijay135 <bijayshah975@gmail.com>
2020-01-28 11:50:41 +0545

    added column logic

--- fetcher.ps1
+++ fetcher.ps1
@@ -1,7 +1,17 @@
 $path = Read-Host -Prompt "Input your full folder path"
+$column =  Read-Host -Prompt "How many columns do you want"
+$count = 1
 
-Get-ChildItem -Path $path -Attribute !D | 
-%{ write-output "$($_.Name)," } |
+Get-ChildItem -Path $path -Attribute !D | %{ 
+    if($count -eq $column){
+        write-output "$($_.Name),`n" 
+        $count = 1
+     }
+    else {
+         write-output "$($_.Name),"
+         $count++;
+    }
+} |
 Out-File folder-contents.txt -NoNewline
 
 write-host "All contents fetched successfully"
